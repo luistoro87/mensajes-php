3049ca70a1971d2ac4f066146f7b472cc495343c
Pedro Luis ToroAcosta <73620793+luistoro87@users.noreply.github.com>
2020-11-05 21:35:06 -0500

    conexion con la base de datos

--- registro.php
+++ registro.php
@@ -1,3 +1,26 @@
+<?php
+
+include 'conexion.php';
+
+
+if (isset($_POST['enviar'])) {
+	//recibimos las variables del formulario
+	$nombre_completo = $_POST['nombre_completo'];
+	$celular = $_POST['celular'];
+	$correo = $_POST['correo'];
+	$clave = $_POST['clave'];
+	$clave2 = $_POST['clave2'];
+
+	//validar nuevamente las contraseñas
+	if ($clave != $clave2 || $clave.length <7) {
+		$respuesta = "Las contraseñas no coinciden o son menores a 7 caracteres";
+	}
+
+	//conectarnos a la BD
+
+}
+
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -7,7 +30,7 @@
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
-		<header><center>
+		<header>
 			<h1>mensajes App</h1>
 			<h2>Registro de usuario</h2>
 		</header>
@@ -18,39 +41,57 @@
 					<div>
 						<p>Si tienes una cuenta creada, <a href="login.php">inicia sesión</a> </p>
 					</div>
-					<form name="login" action="" method="POST">
+					<form name="login" action="" method="POST" onsubmit="return validarClave()">
 					  	<div class="form-group">
 						    <label for="nombre_completo">Nombre completo</label>
-						    <input type="text" class="form-control" id="nombre_completo" name="nombre_completo" aria-describedby="nombre-ayuda">
+						    <input type="text" class="form-control" id="nombre_completo" name="nombre_completo" aria-describedby="nombre-ayuda" required>
 					    	<small id="nombre-ayuda" class="form-text text-muted">Tu nombre completo .</small>
 					  	</div>
+
 					  	<div class="form-group">
 						    <label for="correo">Correo</label>
-						    <input type="email" class="form-control" id="correo" name="correo" aria-describedby="correo-ayuda">
+						    <input type="email" class="form-control" id="correo" name="correo" aria-describedby="correo-ayuda" required>
 					    	<small id="correo-ayuda" class="form-text text-muted">No te enviaremos Spam, allí podrás recuperar tu cuenta.</small>
 					  	</div>
+
 					  	<div class="form-group">
 						    <label for="celular">Celular</label>
-						    <input type="tel" class="form-control" id="celular" name="celular" aria-describedby="celular-ayuda">
+						    <input type="tel" class="form-control" id="celular" name="celular" aria-describedby="celular-ayuda" required>
 					    	<small id="celular-ayuda" class="form-text text-muted">también puedes usar tu número para el login.</small>
 					  	</div>
+
 					  	<div class="form-group">
 					    	<label for="clave">Contraseña</label>
-					    	<input type="password" class="form-control" id="clave" name="clave">
+					    	<input required type="password" class="form-control" id="clave" name="clave">
 					  	</div>
+
 					  	<div class="form-group">
 					    	<label for="clave2">Verifica tu Contraseña</label>
-					    	<input type="password" class="form-control" id="clave2" name="clave2">
+					    	<input required type="password" class="form-control" id="clave2" name="clave2">
 					  	</div>
-					  	<button type="submit" name="enviar" class="btn btn-primary">Enviar</button>
+					  	<button id="enviar" type="submit" name="enviar" class="btn btn-primary"  >Enviar</button>
 					</form>
-					<p>
 				</div>
 				<div class="col-md-3"></div>
 			</div>
+
 		</section>
-		<footer><center>
+		<footer>
 			<p>Mensajes App - creado para mostrar Owasp 2020 ©</p>
 		</footer>
+		<script type="text/javascript">
+
+			function validarClave(){
+				let clave = document.getElementById('clave').value;	
+				let clave2 = document.getElementById('clave2').value;
+
+				if(clave == clave2 && clave.length>7){
+					return true;
+				}else{
+					return false;
+				}
+			}
+
+		</script>
 	</body>
 </html>
